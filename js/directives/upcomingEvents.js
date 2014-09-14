d313e97925c4e015b06f32aae1a7416f6da75b4e
knupur <nupurk15@gmail.com>
2014-09-14 16:30:38 -0400

    Cleaned up directory, added comments

--- js/directives/upcomingEvents.js
+++ js/directives/upcomingEvents.js
@@ -1,3 +1,7 @@
+/* 
+  Naive mistake!! Should have spelled directivive in the name itself, could feel the pain through out the app. 
+*/
+
 app.directive('upcomingEvents', function() {
   return {
     restrict    : 'A', // attribute directive
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var app = angular.module('nkApp', []);
+var app = angular.module('nkApp', []); //the module container object for the application!
 
 var fmBaseURL = "http://ws.audioscrobbler.com/2.0/";
     apiKey = "684817a22a422c8788aaf4a122df990f"
